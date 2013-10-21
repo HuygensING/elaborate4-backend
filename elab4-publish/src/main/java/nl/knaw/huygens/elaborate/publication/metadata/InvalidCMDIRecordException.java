deede42414aa26df749bf2255bcfd9d56c1722d9
BramB <bram.buitendijk@huygens.knaw.nl>
2013-10-21 15:33:20 +0200

    throw InvalidCMDIRecordException

---
+++ elab4-publish/src/main/java/nl/knaw/huygens/elaborate/publication/metadata/InvalidCMDIRecordException.java
@@ -0,0 +1,10 @@
+package nl.knaw.huygens.elaborate.publication.metadata;
+
+public class InvalidCMDIRecordException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public InvalidCMDIRecordException(String message) {
+    super(message);
+  }
+
+}
