317d4279debb942427ac1fe744fa317f3495645c
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2013-10-29 13:16:44 +0100

    redeploy script

--- elab4-backend/scripts/redeploy2testserver.sh
+++ elab4-backend/scripts/redeploy2testserver.sh
@@ -1,6 +1,6 @@
 tag=testdeploy_`date +"%Y%m%d%H%M%S"`
 git tag $tag
-propfile=src/main/resources/version.properties
+propfile=src/main/resources/version.properties;
 echo "build=$tag" > $propfile
 echo "builddate=`date`" >> $propfile
 mvn tomcat:redeploy -P testserver
