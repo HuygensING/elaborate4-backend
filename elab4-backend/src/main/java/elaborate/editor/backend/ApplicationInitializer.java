7f5e92b35750827b79f90c393b0b0a755f45a20d
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2014-03-05 16:55:23 +0100

    starttime: 24-hours format

--- elab4-backend/src/main/java/elaborate/editor/backend/ApplicationInitializer.java
+++ elab4-backend/src/main/java/elaborate/editor/backend/ApplicationInitializer.java
@@ -10,11 +10,14 @@ import nl.knaw.huygens.LoggableObject;
 
 public class ApplicationInitializer extends LoggableObject implements ServletContextListener {
 
+	public ApplicationInitializer() {
+		System.setProperty("application.starttime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+	}
+
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		LOG.info("initializing context");
 		LOG.info("serverinfo={}", sce.getServletContext().getServerInfo());
-		System.setProperty("application.starttime", new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()));
 	}
 
 	@Override
