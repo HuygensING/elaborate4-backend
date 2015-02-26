85745b0fabb535978c93591b1de779f46c7fcc37
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2015-02-26 13:24:44 +0100

    replace newlines in facetvalues

--- elab4-backend/src/test/java/elaborate/editor/solr/ElaborateSolrIndexerTest.java
+++ elab4-backend/src/test/java/elaborate/editor/solr/ElaborateSolrIndexerTest.java
@@ -23,6 +23,8 @@ package elaborate.editor.solr;
  */
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
@@ -36,47 +38,58 @@ import com.google.common.collect.ImmutableList;
 import elaborate.editor.AbstractTest;
 import elaborate.editor.model.orm.Project;
 import elaborate.editor.model.orm.ProjectEntry;
-import elaborate.editor.model.orm.ProjectEntryMetadataItem;
-import elaborate.editor.model.orm.User;
 
 public class ElaborateSolrIndexerTest extends AbstractTest {
 
-	private ProjectEntry entry;
+	//	private ProjectEntry entry;
 
-	//	@Before
-	public void setUp() throws Exception {
-		Project project = new Project();
-		List<ProjectEntryMetadataItem> projectEntryMetadataItems = ImmutableList.<ProjectEntryMetadataItem> of();
-		User creator = new User();
-		entry = project.addEntry("brief 1", creator).setProjectEntryMetadataItems(projectEntryMetadataItems);
-		entry.addTranscription(creator)//
-				.setBody("<body><ab id=\"9085822\"/>sdgdgdgsdgsdfg<ae id=\"9085822\"/></body>")//
-				.setTextLayer("layer1");
-		entry.addTranscription(creator)//
-				.setBody("<body>aap <i>noot</i> mies</body>")//
-				.setTextLayer("layer2");
-	}
+	//	//	@Before
+	//	public void setUp() throws Exception {
+	//		Project project = new Project();
+	//		List<ProjectEntryMetadataItem> projectEntryMetadataItems = ImmutableList.<ProjectEntryMetadataItem> of();
+	//		User creator = new User();
+	//		entry = project.addEntry("brief 1", creator).setProjectEntryMetadataItems(projectEntryMetadataItems);
+	//		entry.addTranscription(creator)//
+	//				.setBody("<body><ab id=\"9085822\"/>sdgdgdgsdgsdfg<ae id=\"9085822\"/></body>")//
+	//				.setTextLayer("layer1");
+	//		entry.addTranscription(creator)//
+	//				.setBody("<body>aap <i>noot</i> mies</body>")//
+	//				.setTextLayer("layer2");
+	//	}
 
-	//	@After
-	public void tearDown() throws Exception {}
+	@Test
+	public void test_newlines_in_entry_metadata_are_replaced() throws Exception {
+		// Setup mock project
+		Project mockProject = mock(Project.class);
+		when(mockProject.getId()).thenReturn(2l);
+		when(mockProject.getProjectEntryMetadataFieldnames()).thenReturn(ImmutableList.of("multiline"));
 
-	//	@Test
-	public void testGetSolrInputDocument() throws Exception {
-		SolrInputDocument docForEditor = ElaborateSolrIndexer.getSolrInputDocument(entry, false);
+		// Setup mock projectentry
+		ProjectEntry mockEntry = mock(ProjectEntry.class);
+		when(mockEntry.getId()).thenReturn(1l);
+		when(mockEntry.getName()).thenReturn("name");
+		when(mockEntry.getProject()).thenReturn(mockProject);
+		when(mockEntry.isPublishable()).thenReturn(true);
+		when(mockEntry.getMetadataValue("multiline")).thenReturn("Metadata\nvalues\nwith\nmultiple\nlines");
+
+		SolrInputDocument docForEditor = ElaborateSolrIndexer.getSolrInputDocument(mockEntry, false);
 		assertThat(docForEditor != null).isTrue();
 		LOG.info("docForEditor={}", docForEditor);
-		assertThat(docForEditor.getField(SolrFields.ID).getValue()).isEqualTo(entry.getId());
-		assertThat(docForEditor.getField(SolrFields.NAME).getValue()).isEqualTo(entry.getName());
-		assertThat(docForEditor.getField(SolrFields.PROJECT_ID).getValue()).isEqualTo(entry.getProject().getId());
-		assertThat(docForEditor.getField(SolrFields.PUBLISHABLE).getValue()).isEqualTo(entry.isPublishable());
+		assertThat(docForEditor.getField(SolrFields.ID).getValue()).isEqualTo(mockEntry.getId());
+		assertThat(docForEditor.getField(SolrFields.NAME).getValue()).isEqualTo(mockEntry.getName());
+		assertThat(docForEditor.getField(SolrFields.PROJECT_ID).getValue()).isEqualTo(mockEntry.getProject().getId());
+		assertThat(docForEditor.getField(SolrFields.PUBLISHABLE).getValue()).isEqualTo(mockEntry.isPublishable());
+		assertThat(docForEditor.getField("metadata_multiline").getValue()).isEqualTo("Metadata/values/with/multiple/lines");
 
-		SolrInputDocument docForPublication = ElaborateSolrIndexer.getSolrInputDocument(entry, true);
+		when(mockEntry.getMetadataValue("multiline")).thenReturn("Metadata\r\nvalues\r\nwith\r\nmultiple\r\nlines");
+		SolrInputDocument docForPublication = ElaborateSolrIndexer.getSolrInputDocument(mockEntry, true);
 		assertThat(docForPublication != null).isTrue();
 		LOG.info("docForPublication={}", docForPublication);
-		assertThat(docForPublication.getField(SolrFields.ID).getValue()).isEqualTo(entry.getId());
-		assertThat(docForPublication.getField(SolrFields.NAME).getValue()).isEqualTo(entry.getName());
+		assertThat(docForPublication.getField(SolrFields.ID).getValue()).isEqualTo(mockEntry.getId());
+		assertThat(docForPublication.getField(SolrFields.NAME).getValue()).isEqualTo(mockEntry.getName());
 		assertThat(docForPublication.getField(SolrFields.PROJECT_ID)).isEqualTo(null);
 		assertThat(docForPublication.getField(SolrFields.PUBLISHABLE)).isEqualTo(null);
+		assertThat(docForEditor.getField("metadata_multiline").getValue()).isEqualTo("Metadata/values/with/multiple/lines");
 	}
 
 	@Test
