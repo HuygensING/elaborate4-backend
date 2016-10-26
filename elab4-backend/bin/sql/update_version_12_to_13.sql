e32e4bc43dd05348a30c0399953e8a910f899eba
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2016-10-26 11:07:15 +0200

    remove */bin

--- elab4-backend/bin/sql/update_version_12_to_13.sql
+++
@@ -1,4 +0,0 @@
-alter table project_entries add short_name text not null default '';
-update project_entries set short_name = substring(name from 1 for 8);
-
-update version set version = '13' WHERE version='12';
