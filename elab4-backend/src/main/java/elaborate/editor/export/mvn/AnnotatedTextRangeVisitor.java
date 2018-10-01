e0e7421b820cab685a4ea5a0a3cb70bb5ff1cb38
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2018-10-01 12:03:52 +0200

    Update headers

--- elab4-backend/src/main/java/elaborate/editor/export/mvn/AnnotatedTextRangeVisitor.java
+++ elab4-backend/src/main/java/elaborate/editor/export/mvn/AnnotatedTextRangeVisitor.java
@@ -1,20 +1,33 @@
 package elaborate.editor.export.mvn;
 
+/*
+ * #%L
+ * elab4-backend
+ * =======
+ * Copyright (C) 2011 - 2018 Huygens ING
+ * =======
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import com.google.common.collect.Sets;
+import nl.knaw.huygens.tei.*;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Set;
-
-import com.google.common.collect.Sets;
-
-import nl.knaw.huygens.tei.Comment;
-import nl.knaw.huygens.tei.CommentHandler;
-import nl.knaw.huygens.tei.DelegatingVisitor;
-import nl.knaw.huygens.tei.Element;
-import nl.knaw.huygens.tei.ElementHandler;
-import nl.knaw.huygens.tei.Text;
-import nl.knaw.huygens.tei.TextHandler;
-import nl.knaw.huygens.tei.Traversal;
-import nl.knaw.huygens.tei.XmlContext;
 
 public class AnnotatedTextRangeVisitor extends DelegatingVisitor<XmlContext> implements ElementHandler<XmlContext>, TextHandler<XmlContext>, CommentHandler<XmlContext> {
   private final Set<RangeAnnotation> rangeAnnotations = Sets.newLinkedHashSet();
