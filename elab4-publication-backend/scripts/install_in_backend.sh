3cdf87b26950c414dd4a9b945658d36c84fb6811
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2018-11-05 13:54:49 +0100

    wrap variable

--- elab4-publication-backend/scripts/install_in_backend.sh
+++ elab4-publication-backend/scripts/install_in_backend.sh
@@ -5,5 +5,5 @@ tag=publication_backend_`date +"%Y.%m.%d.%H.%M.%S"`
 git tag ${tag}
 # echo "build=$tag" > $propfile
 # echo "builddate=`date`" >> $propfile
-rm $dest/WEB-INF/lib/*.jar
+rm ${dest}/WEB-INF/lib/*.jar
 mvn clean compile war:exploded && rsync -cav target/elab4-publication-backend*/* $dest/
