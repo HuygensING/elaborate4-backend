d41fd1f6e479bf740a620832baf4061dfd1fc20b
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2018-11-06 16:13:23 +0100

    Use LoggerFactory for main code, fix some deprecation messages

--- elab4-common/src/main/java/nl/knaw/huygens/facetedsearch/RemoteSolrServer.java
+++ elab4-common/src/main/java/nl/knaw/huygens/facetedsearch/RemoteSolrServer.java
@@ -1,6 +1,8 @@
 package nl.knaw.huygens.facetedsearch;
 
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /*
  * #%L
@@ -12,36 +14,35 @@ import org.apache.solr.client.solrj.impl.HttpSolrServer;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
 
-import nl.knaw.huygens.Log;
-
 public class RemoteSolrServer extends AbstractSolrServer {
-	private final String solrUrl;
+  private Logger LOG = LoggerFactory.getLogger(getClass());
+  private final String solrUrl;
 
-	public RemoteSolrServer(String solrUrl, QueryComposer queryComposer) {
-		super(queryComposer);
-		this.solrUrl = solrUrl;
-		setServer();
-	}
+  public RemoteSolrServer(String solrUrl, QueryComposer queryComposer) {
+    super(queryComposer);
+    this.solrUrl = solrUrl;
+    setServer();
+  }
 
-	@Override
-	public void setServer() {
-		Log.info("SOLR URL = {}", solrUrl);
-		server = new HttpSolrServer(solrUrl);
-	}
+  @Override
+  public void setServer() {
+    LOG.info("SOLR URL = {}", solrUrl);
+    server = new HttpSolrServer(solrUrl);
+  }
 
-	public String getUrl() {
-		return solrUrl;
-	}
+  public String getUrl() {
+    return solrUrl;
+  }
 }
