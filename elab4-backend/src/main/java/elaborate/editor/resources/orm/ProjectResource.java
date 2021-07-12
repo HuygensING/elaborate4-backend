cda4c359b51918c6946479095e802a2b923fb0bf
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2021-07-12 15:30:39 +0200

    update copyright headers

--- elab4-backend/src/main/java/elaborate/editor/resources/orm/ProjectResource.java
+++ elab4-backend/src/main/java/elaborate/editor/resources/orm/ProjectResource.java
@@ -4,18 +4,18 @@ package elaborate.editor.resources.orm;
  * #%L
  * elab4-backend
  * =======
- * Copyright (C) 2011 - 2019 Huygens ING
+ * Copyright (C) 2011 - 2021 Huygens ING
  * =======
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
