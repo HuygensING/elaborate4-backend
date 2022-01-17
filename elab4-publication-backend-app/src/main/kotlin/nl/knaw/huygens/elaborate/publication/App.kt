fe9e971767bdf4355bb6fab0c2d9911414cfa880
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-01-17 16:22:14 +0100

    add publication-backend app: initial setup

---
+++ elab4-publication-backend-app/src/main/kotlin/nl/knaw/huygens/elaborate/publication/App.kt
@@ -0,0 +1,29 @@
+package nl.knaw.huygens.elaborate.publication
+
+import io.dropwizard.Application
+import io.dropwizard.setup.Environment
+import nl.knaw.huygens.elaborate.publication.resources.AboutResource
+import nl.knaw.huygens.elaborate.publication.resources.DataResource
+import nl.knaw.huygens.elaborate.publication.resources.RootResource
+import nl.knaw.huygens.elaborate.publication.resources.SearchResource
+
+class App : Application<AppConfig>() {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            App().run(*args)
+        }
+    }
+
+    override fun getName(): String {
+        return "elab4-publication-app"
+    }
+
+    override fun run(config: AppConfig, env: Environment) {
+        env.jersey().register(RootResource())
+        env.jersey().register(AboutResource(config))
+        env.jersey().register(DataResource(config))
+        env.jersey().register(SearchResource::class.java)
+    }
+}
